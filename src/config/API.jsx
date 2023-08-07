eb85795779185bef55d08adc2aa1164a8c78bec1
Ramli S Siburian <ramlisiburian1997@gmail.com>
2023-08-08 00:19:32 +0700

    update env

--- src/config/API.jsx
+++ src/config/API.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const API = axios.create({
-    baseURL : 'http://localhost:5000/api/v1'
+    baseURL : process.env.REACT_APP_BASEURL || 'http://localhost:5000/api/v1'
 })
 
 export const setAuthToken = (token) => {
