61a01909cc300ba577d45dcc69bfe596f38e047e
Ramli <91863695+RamliSiburian@users.noreply.github.com>
2023-08-23 19:01:24 +0700

    api

--- src/config/API.jsx
+++ src/config/API.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const API = axios.create({
-    baseURL : process.env.RREACT_APP_BASEURL || 'http://localhost:5000/api/v1'
+    baseURL: 'https://mcdm.up.railway.app/api/v1/'
+    // baseURL : process.env.RREACT_APP_BASEURL || 'http://localhost:5000/api/v1'
     //  || 'http://localhost:5000/api/v1'
     // 'https://mcdm.up.railway.app/api/v1/
 })
