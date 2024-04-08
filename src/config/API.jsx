a28cb44ead2b5a006c915738994c2c63ffebafac
Ramli Siburian <ramlisiburian1997@gmail.com>
2024-04-08 17:31:13 +0700

    update base url

--- src/config/API.jsx
+++ src/config/API.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 export const API = axios.create({
-  baseURL: 'https://103.250.11.139/api/v1/',
+  baseURL: "https://103.13.206.206/api/v1/",
   // baseURL : process.env.RREACT_APP_BASEURL || 'http://localhost:5000/api/v1'
   //  baseURL : 'http://localhost:5000/api/v1'
   // 'https://mcdm.up.railway.app/api/v1/
@@ -8,8 +8,8 @@ export const API = axios.create({
 
 export const setAuthToken = (token) => {
   if (token) {
-    API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
-    delete API.defaults.headers.common['Authorization'];
+    delete API.defaults.headers.common["Authorization"];
   }
 };
